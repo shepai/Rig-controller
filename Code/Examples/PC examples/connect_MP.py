f14e9562f479561bf07c17df9706d4c40e795492
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-04-11 16:08:50 +0100

    Update connect_MP.py

--- Code/Examples/PC examples/connect_MP.py
+++ Code/Examples/PC examples/connect_MP.py
@@ -6,7 +6,14 @@ import time
 import Controller
 
 path="C:/Users/dexte/Documents/GitHub/Rig-controller/Code/Examples/Board Examples/listener_MP.py"
-c= Controller.Controller('COM15',file=path)
+c= Controller.Controller('COM19',file=path)
 
 print(c.getPressure())
-c.move(10,10,10,10)
+#c.move(10,10,10,10)
+c.calibrate() #takes a while - only want to do once
+#c.sendCommand("CALIB") #SAY THAT IT IS ALREADY Calibrated
+print(c.sendCommand("getmove"))
+c.move(500,100,0,0) #move to new space
+print(c.sendCommand("getmove"))
+c.reset_trial() #return to center position
+print(c.sendCommand("getmove"))
