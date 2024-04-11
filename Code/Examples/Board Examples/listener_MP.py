747297d77d3834599045c55a0c20770363076ddf
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-04-11 15:47:26 +0100

    Update listener_MP.py

--- Code/Examples/Board Examples/listener_MP.py
+++ Code/Examples/Board Examples/listener_MP.py
@@ -1,7 +1,7 @@
 from Rig_control import Rig, client
+import time
 
-c=client() #create client
-rig=Rig() #create rig
+rig=Rig(plate_mode=1) #create rig
 
 def pressure():
     s=""
@@ -9,11 +9,24 @@ def pressure():
     if type(vals)==type([]):
         for char in vals:
             s+=str(char)+","
-        print(s[:,-1]) #send data of pads back
+        print(s[:-1]) #send data of pads back
     else:
         print(vals)
 def move(x,y,z,a):
-    array=[a,z,y,x]
+    array=[x,y,z,a]
     rig.moveMotors(*array) #move the motors
 def reset():
-    rig.resetRig()
+    rig.reset()
+
+#1500
+#3000
+#z auto
+#rig.reset()
+#rig.lowerSensor()
+#calibrate
+#rig.reset()
+#print("reset")
+#rig.central()
+
+
+
