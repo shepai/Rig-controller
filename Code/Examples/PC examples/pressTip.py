cfbc349f48a13ef093464b0766876c8d1b7bca9f
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-04-15 17:26:14 +0100

    Update pressTip.py

--- Code/Examples/PC examples/pressTip.py
+++ Code/Examples/PC examples/pressTip.py
@@ -29,11 +29,10 @@ def runTrial(SAVER,dirs=[0,0]):
     t1=time.time()
     x_vector=10*dirs[0]
     y_vector=10*dirs[1]
-    print("vector",x_vector,y_vector)
     for i in range(0,100):
         c.move(x_vector,y_vector,0,0)
         data_sensor=list(B.getSensor(type_="round",num=16))
-        SAVER.upload(data_sensor,time.time()-t1,dirs+[0,0])
+        SAVER.upload(data_sensor,time.time()[x_vector+i,y_vector+i]+[0,0])
 
 #####################
 #Experiment hyperparameters
@@ -44,16 +43,17 @@ angle=0
 speed=100
 texture="none"
 experiment=dx.Experiment(0,texture,80,20)
+starttime=time.time()
 for exp in range(num_experiments):
     experiment.create_experiment(exp,texture,angle,speed)
     for trial in range(num_of_trials): #gives you the ability to average over number of trials
         print("Experiment",exp+1,"Trial",trial+1)
-        for y in np.arange(0,1,0.05): #move y along surface 
-            for x in reversed(np.arange(0,1,0.05)): #move direction of x along
+        for y in np.arange(0,1,0.1): #move y along surface 
+            for x in reversed(np.arange(0,1,0.1)): #move direction of x along
                 experiment.create_trial()
                 runTrial(experiment,dirs=[x,y]) #send vector through
-                c.move(0,0,100,0)
+                c.move(0,0,500,0)
             experiment.save(path_to_save) #constant backups
 
 
-
+print("EXECUTION TIME:",time.time()-starttime)
