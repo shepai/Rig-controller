bf843a546a3547fef45374feb5b875500b31a26d
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-05-07 16:07:39 +0100

    Update TacTip.py

--- Code/Examples/PC examples/TacTip.py
+++ Code/Examples/PC examples/TacTip.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 path="C:/Users/dexte/Documents/GitHub/Rig-controller/Code/Examples/Board Examples/listener_MP.py"
-path_to_save="C:/Users/dexte/Documents/AI/XML_sensors/TacTip_Flat"
+path_to_save="C:/Users/dexte/Documents/AI/XML_sensors/TacTip_Flat_P10"
 c= Controller.Controller('COM19',file=path)
 THRESH=6000
 Pressure_extra=0
@@ -32,7 +32,7 @@ sensor=[]
 def runTrial(SAVER,dirs=[0,0]):
     global sensor
     c.reset_trial() #return to center position
-    c.move(EDGE_VALUE+0,50,50,0)
+    c.move(EDGE_VALUE+0,50,50-FORCE,0)
     #move sensor across surface
     t1=time.time()
     x_vector=10*dirs[0]
@@ -43,7 +43,7 @@ def runTrial(SAVER,dirs=[0,0]):
         if not ret:
             print("incorrect")
             frame=np.zeros((100,100,1))
-        data_sensor=list(frame.flatten)
+        data_sensor=list(frame.flatten())
         SAVER.upload(data_sensor,time.time(),[x_vector+i,y_vector+i]+[0,0])
         sensor.append(data_sensor)
         if len(sensor)>100: #prevent too many values
@@ -56,14 +56,15 @@ def runTrial(SAVER,dirs=[0,0]):
 #Experiment hyperparameters
 ####################
 num_experiments=1
-num_of_trials=5
+num_of_trials=3
 angle=0
 speed=100
-texture="C40"
+texture="Plastic"
 experiment=dx.Experiment(0,texture,angle,speed)
 starttime=time.time()
 total_operations=num_of_trials*num_experiments
 EDGE_VALUE=0
+FORCE=10 #0, 10, 20, 30, 40, 50, 60, 70, 80 ,90
 
 for exp in range(num_experiments):
     experiment.create_experiment(exp,texture,angle,speed)
