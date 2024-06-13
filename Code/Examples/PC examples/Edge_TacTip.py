8f1d56b4e55e874077920e47b1ddf4207929b072
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-06-13 10:02:55 +0100

    rename

--- Code/Examples/PC examples/Edge_TacTip.py
+++ Code/Examples/PC examples/Edge_TacTip.py
@@ -10,8 +10,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-def readSensor():
-    pass
+
 def Experiment(name,FORCE):
     path="C:/Users/dexte/Documents/GitHub/Rig-controller/Code/Examples/Board Examples/listener_MP.py"
     path_to_save="C:/Users/dexte/Documents/AI/XML_sensors/"
@@ -31,7 +30,7 @@ def Experiment(name,FORCE):
     c.calibrate(value=THRESH,lower=False,val=-500-Pressure_extra) #takes a while - only want to do once
     c.move(0,-3000,100,0) #0,-3000,-100,0
     num_experiments=1
-    num_of_trials=2
+    num_of_trials=4
     angle=0
     speed=100
     texture="Plastic"
@@ -43,27 +42,43 @@ def Experiment(name,FORCE):
         exit()
     ret, frame = cap.read()
     print("COMPONENTS",frame.shape)
-    #experiment=dx.Experiment(0,texture,angle,speed)
+    experiment=dx.Experiment(0,texture,angle,speed)
     starttime=time.time()
     total_operations=(num_of_trials*(len(np.arange(0,1,0.1))**2))*num_experiments
     EDGE_VALUE=0
     for exp in range(num_experiments):
-        #experiment.create_experiment(exp,texture,angle,speed)
+        experiment.create_experiment(exp,texture,angle,speed)
         for trial in range(num_of_trials): #gives you the ability to average over number of trials
             total_operations_left=(num_experiments-exp)*(num_of_trials-trial)
             #for rotation in range(0,100,20):
                 #c.move(0,0,0,rotation) #0,-3000,-100,0
+            ar=[]
             for i in range(10):
                 #move down
-                c.move(0,0,-1*(100+FORCE),0) #0,-3000,-100,0
+                c.move(0,0,-1*(200+FORCE),0) #0,-3000,-100,0
                 #record
+                ret, frame = cap.read()
+                if not ret:
+                    print("incorrect")
+                    frame=np.zeros((100,100,1))
+                cv2.imshow('TacTip Feed', frame)
+                # Check for the 'q' key to exit the loop
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                data_sensor=list(frame.flatten())
+                ar.append(data_sensor)
                 print("record",30+(2*i),"% on edge")
                 time.sleep(1)
                 #move up and along
-                c.move(0,20,(100+FORCE),0) 
+                c.move(0,20,(200+FORCE),0) 
             c.move(0,20,500,0) 
             c.reset_trial(centering=False)
             c.move(0,-3000,100,0) #0,-3000,-100,0
+            ar=np.array(ar)
+            filename=str(trial)+"-"+str(i)
+            np.savez_compressed(path_to_save+"_"+filename,ar)
+            experiment.upload(filename,time.time(),[0,0]+[0,0])
+        experiment.save(path_to_save) #constant backups
                 
 
 Experiment("test",0)
