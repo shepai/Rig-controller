9da43a17071ede631ab5288609f5e457472650d7
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2025-02-18 19:09:49 +0000

    Update Rig.h

--- Code/boardSide/Arduino/Rig_control_Arduino/Rig.h
+++ Code/boardSide/Arduino/Rig_control_Arduino/Rig.h
@@ -17,7 +17,7 @@ class RigControl {
     int buttonX = 7;    // pushbutton connected to digital pin 
     int buttonY = 7;    // pushbutton connected to digital pin
     int buttonZ = 7;    // pushbutton connected to digital pin
-    int positions[];
+    int* positions;
   public:
     // Constructor
     RigControl() {
@@ -27,6 +27,7 @@ class RigControl {
       myMotorA = kit1.getStepper(200, 2);
       myMotorB = kit1.getStepper(200, 1);
       myMotorC = kit2.getStepper(200, 2);
+      int set_value = {10,10,10};
       int positions[] ={0,0,0};
     }
     void setSpeeds(int rpm1, int rpm2, int rpm3) {
@@ -34,20 +35,37 @@ class RigControl {
       myMotorB->setSpeed(rpm2);
       myMotorC->setSpeed(rpm3);
     }
-    void move(int x, int y, int z) {
-      if(x>0){myMotorA->step(abs(x), FORWARD, SINGLE); }
+    void move(int x, int y, int z) { // move the steppers based on direction, but not if pushing against wall
+      int* states = readButtons();
+      if(x>0 && states[0]==0){myMotorA->step(abs(x), FORWARD, SINGLE); }
       else{myMotorA->step(abs(x), BACKWARD, SINGLE); }
-      if(y>0){myMotorB->step(abs(y), FORWARD, SINGLE); }
+      if(y>0 && states[1]==0){myMotorB->step(abs(y), FORWARD, SINGLE); }
       else{myMotorB->step(abs(y), BACKWARD, SINGLE); }
-      if(z>0){myMotorC->step(abs(z), FORWARD, SINGLE); }
+      if(z>0 && states[2]==0){myMotorC->step(abs(z), FORWARD, SINGLE); }
       else{myMotorC->step(abs(z), BACKWARD, SINGLE); }
+      //update memory of position
+      positions[0]+=x;
+      positions[1]+=y;
+      positions[2]+=z;
     }
-    void zero(){
+    void zero(){ // return to states
+      int moveX=positions[0]*-1;
+      int moveY=positions[1]*-1;
+      int moveZ=positions[2]*-1;
+      move(moveX,moveY,moveZ)
+      positions={0,0,0};
+    }
+    int *readButtons() {
+      static int buttonStates[3];  // Static array to keep data after function exits
+      buttonStates[0] = digitalRead(buttonX);
+      buttonStates[1] = digitalRead(buttonY);
+      buttonStates[2] = digitalRead(buttonZ);
+      return buttonStates;  // Return the array
+  }
+  void recalibrate() {
+    move(set_value[0],set_value[1],set_value[2]);
+  }
 
-    }
-    int * readButtons() {
-
-    }
 };
 
 #endif
