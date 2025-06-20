c549317846147355deed9d78fda8494695c2efce
shepai <dextershepherd@hotmail.co.uk>
2025-06-20 17:04:07 +0100

    updates

--- Code/Examples/PC examples/efficient_image_record.py
+++ Code/Examples/PC examples/efficient_image_record.py
@@ -9,31 +9,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import TactileSensor as ts
 import os
+clear = lambda: os.system('clear')
 
 class experiment:
-    def __init__(self,name,FORCE,presstip=False):
-        clear = lambda: os.system('clear')
+    def __init__(self,presstip=False,Pressure_extra=-480):
+        
         path="/home/dexter/Documents/Rig-controller/Code/Examples/Board Examples/listener_MP.py"
-        path_to_save="/home/dexter/Documents/data/"
-        c= Controller.Controller("/dev/ttyACM0",file=path)
+        self.path_to_save="/home/dexter/Documents/data/"
+        self.c= Controller.Controller("/dev/ttyACM1",file=path)
         THRESH=6000
-        Pressure_extra=-480  #-480 for normal -880 for foam -230 for cork and carpets
+        #Pressure_extra=-480  #-480 for normal -880 for foam -230 for cork and carpets
         #####################
         # Set up secondary sensor
         ####################
         print("Connecting to sensor")
         connected=False
         i=0
+        self.cap=None
         if not presstip:
             while not connected:
-                cap = cv2.VideoCapture(i)
-                if not cap.isOpened():
+                self.cap = cv2.VideoCapture(i)
+                if not self.cap.isOpened():
                     print("Error: Unable to access the webcam.")
                     i+=1
                 else:
                     print("CAmera channel",i)
                     connected=True
-            ret, frame = cap.read()
+            ret, self.frame = self.cap.read()
         else: #presstip is there
             self.B=ts.Board()
             #get serial boards and connect to first one
@@ -42,13 +44,32 @@ class experiment:
             print("Running file")
             self.B.runFile("/home/dexter/Documents/TactileSensor/Code/TactileSensor/Board side/boardSide.py")
             print("File ran")
-            frame=np.array(list(self.B.getSensor(type_="round",num=16)))#
-            Pressure_extra-=400
+            self.frame=np.array(list(self.B.getSensor(type_="round",num=16)))#
+            Pressure_extra-=350
         self.presstip=presstip
-        c.calibrate(value=THRESH,lower=False,val=Pressure_extra) #takes a while - only want to do once
-        print("COMPONENTS",frame.shape)
-
-
+        
+        self.pressure_extra=Pressure_extra
+        self.c.calibrate(value=THRESH,lower=False,val=Pressure_extra) #takes a while - only want to do once
+        print("COMPONENTS",self.frame.shape)
+    def getCamera(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            print("incorrect")
+            i=0
+            while not ret:
+                self.cap = cv2.VideoCapture(i)
+                if not self.cap.isOpened():
+                    print("Error: Unable to access the webcam.")
+                    i+=1
+                else:
+                    print("CAmera channel",i)
+                    connected=True
+                    ret, frame = self.cap.read()
+                if i>10: i=0
+            
+        return frame
+    def runLinear(self,name,FORCE):
+        self.FORCE=FORCE
         #####################
         #Experiment hyperparameters
         ####################
@@ -60,27 +81,21 @@ class experiment:
         def runTrial(SAVER,dirs=[0,0]):
                     global sensor
                     global filename
-                    c.reset_trial() #return to center position
-                    c.move(EDGE_VALUE+0,50,50-FORCE,0)
+                    self.c.reset_trial() #return to center position
+                    self.c.move(EDGE_VALUE+0,50,50-self.FORCE,0)
                     #move sensor across surface
                     t1=time.time()
                     x_vector=10*dirs[0]
                     y_vector=10*dirs[1]
                     if not self.presstip:
-                        ret, frame = cap.read()
-                        if not ret:
-                            print("incorrect")
-                            frame=np.zeros((100,100))
+                        frame=self.getCamera()
                     else:
                         frame=np.array(list(self.B.getSensor(type_="round",num=16)))
                     ar=np.zeros((50,*frame.shape),dtype=np.uint8)
                     for i in range(0,50):
-                        c.move(x_vector,y_vector,0,0)
+                        self.c.move(x_vector,y_vector,0,0)
                         if not self.presstip:
-                            ret, frame = cap.read()
-                            if not ret:
-                                print("incorrect")
-                                frame=np.zeros((100,100,1))
+                            frame=self.getCamera()
                         else:
                             frame=np.array(list(self.B.getSensor(type_="round",num=16)))
                         ar[i]=frame
@@ -90,7 +105,7 @@ class experiment:
                         if cv2.waitKey(1) & 0xFF == ord('q'):
                             break
                     return ar
-        data=np.zeros((num_experiments,num_of_trials,len(np.arange(0,1,0.1)),len(np.arange(0,1,0.1)),50,*frame.shape),dtype=np.uint8)
+        data=np.zeros((num_experiments,num_of_trials,len(np.arange(0,1,0.1)),len(np.arange(0,1,0.1)),50,*self.frame.shape),dtype=np.uint8)+1
         for exp in range(num_experiments):
             for trial in range(num_of_trials): #gives you the ability to average over number of trials
                 for i,y in enumerate(np.arange(0,1,0.1)): #move y along surface 
@@ -105,58 +120,20 @@ class experiment:
 
                             dat=runTrial(experiment,dirs=[x,y]) #send vector through
                             data[exp][trial][i][j]=dat.copy()
-                            c.move(0,0,500,0)
+                            self.c.move(0,0,500,0)
                         except KeyboardInterrupt:
-                            c.reset_trial()
-                            c.move(0,0,1000,0)
+                            self.c.reset_trial()
+                            self.c.move(0,0,1000,0)
                             print("Paused... do you want to continue (ENTER yes ctrl-C no)")
                             input(">")
                 if trial%10==0:
-                    np.save(path_to_save+"/"+name,data) #constant backups
-
+                    np.save(self.path_to_save+"/"+name,data) #constant backups
+        del data
         #plt.show()
         print("TOTAL EXECUTION TIME:",(time.time()-starttime)/(60*60),"hours")
 
-
-class experiment_circle:
-    def __init__(self,name,FORCE,presstip=False):
-        clear = lambda: os.system('clear')
-        path="/home/dexter/Documents/Rig-controller/Code/Examples/Board Examples/listener_MP.py"
-        path_to_save="/home/dexter/Documents/data/"
-        c= Controller.Controller("/dev/ttyACM0",file=path)
-        THRESH=6000
-        Pressure_extra=-500  #-480 for normal -880 for foam -230 for cork and carpets -500 for flat
-        #####################
-        # Set up secondary sensor
-        ####################
-        print("Connecting to sensor")
-        if not presstip:
-            connected=False
-            i=0
-            while not connected:
-                cap = cv2.VideoCapture(i)
-                if not cap.isOpened():
-                    print("Error: Unable to access the webcam.")
-                    i+=1
-                else:
-                    print("CAmera channel",i)
-                    connected=True
-            ret, frame = cap.read()
-        else:
-            self.B=ts.Board()
-            #get serial boards and connect to first one
-            print("Connecting to sensor")
-            self.B.connect("/dev/ttyACM2")
-            print("Running file")
-            self.B.runFile("/home/dexter/Documents/TactileSensor/Code/TactileSensor/Board side/boardSide.py")
-            print("File ran")
-            frame=np.array(list(self.B.getSensor(type_="round",num=16)))
-            Pressure_extra-=400
-        self.presstip=presstip
-        c.calibrate(value=THRESH,lower=False,val=Pressure_extra) #takes a while - only want to do once
-        print("COMPONENTS",frame.shape)
-
-
+    def runCircle(self,name,FORCE):
+        self.FORCE=FORCE
         #####################
         #Experiment hyperparameters
         ####################
@@ -168,27 +145,22 @@ class experiment_circle:
         def runTrial(SAVER,radius):
                     global sensor
                     global filename
-                    c.reset_trial() #return to center position
-                    c.move(EDGE_VALUE+0,50,50-FORCE,0)
+                    self.c.reset_trial() #return to center position
+                    self.c.move(EDGE_VALUE+0,50,50-self.FORCE,0)
                     #move sensor across surface
+                    global cap
                     t1=time.time()
                     if not self.presstip:
-                        ret, frame = cap.read()
-                        if not ret:
-                            print("incorrect")
-                            frame=np.zeros((100,100))
+                        frame=self.getCamera()
                     else:
                         frame=np.array(list(self.B.getSensor(type_="round",num=16)))
                     ar=np.zeros((50,*frame.shape),dtype=np.uint8)
                     for i,t in enumerate(np.arange(0,5,0.1)):
                         x = radius * np.sin(t)
                         y = radius * np.cos(t)
-                        c.move(x,y,0,0)
+                        self.c.move(x,y,0,0)
                         if not self.presstip:
-                            ret, frame = cap.read()
-                            if not ret:
-                                print("incorrect")
-                                frame=np.zeros((100,100))
+                            frame=self.getCamera()
                         else:
                             frame=np.array(list(self.B.getSensor(type_="round",num=16)))
                         ar[i]=frame
@@ -198,7 +170,7 @@ class experiment_circle:
                         if cv2.waitKey(1) & 0xFF == ord('q'):
                             break
                     return ar
-        data=np.zeros((num_experiments,num_of_trials,len(np.arange(10,400,40)),50,*frame.shape),dtype=np.uint8)
+        data=np.zeros((num_experiments,num_of_trials,len(np.arange(10,400,40)),50,*self.frame.shape),dtype=np.uint8)
         for exp in range(num_experiments):
             for trial in range(num_of_trials): #gives you the ability to average over number of trials
                 for i,y in enumerate(np.arange(10,100,10)): #chose radius
@@ -212,60 +184,20 @@ class experiment_circle:
 
                             dat=runTrial(experiment,y) #send vector through
                             data[exp][trial][i]=dat.copy()
-                            c.move(0,0,500,0)
+                            self.c.move(0,0,500,0)
                         except KeyboardInterrupt:
-                            c.reset_trial()
-                            c.move(0,0,1000,0)
+                            self.c.reset_trial()
+                            self.c.move(0,0,1000,0)
                             print("Paused... do you want to continue (ENTER yes ctrl-C no)")
                             input(">")
                 if trial%10==0:
-                    np.save(path_to_save+"/"+name,data) #constant backups
-
+                    np.save(self.path_to_save+"/"+name,data) #constant backups
+        del data
         #plt.show()
         print("TOTAL EXECUTION TIME:",(time.time()-starttime)/(60*60),"hours")
-
-
-
-class experiment_pressure:
-    def __init__(self,name,FORCE,presstip=False):
-        clear = lambda: os.system('clear')
-        path="/home/dexter/Documents/Rig-controller/Code/Examples/Board Examples/listener_MP.py"
-        path_to_save="/home/dexter/Documents/data/"
-        c= Controller.Controller("/dev/ttyACM0",file=path)
-        THRESH=6000
-        Pressure_extra=-500  #-480 for normal -880 for foam -230 for cork and carpets, -500 flat
-        
-        #####################
-        # Set up secondary sensor
-        ####################
-        print("Connecting to sensor")
-        if not presstip:
-            connected=False
-            i=0
-            while not connected:
-                cap = cv2.VideoCapture(i)
-                if not cap.isOpened():
-                    print("Error: Unable to access the webcam.")
-                    i+=1
-                else:
-                    print("CAmera channel",i)
-                    connected=True
-            ret, frame = cap.read()
-        else:
-            self.B=ts.Board()
-            #get serial boards and connect to first one
-            print("Connecting to sensor")
-            self.B.connect("/dev/ttyACM2")
-            print("Running file")
-            self.B.runFile("/home/dexter/Documents/TactileSensor/Code/TactileSensor/Board side/boardSide.py")
-            print("File ran")
-            frame=np.array(list(self.B.getSensor(type_="round",num=16)))
-            Pressure_extra-=400
-        self.presstip=presstip
-        c.calibrate(value=THRESH,lower=False,val=Pressure_extra) #takes a while - only want to do once
-        print("COMPONENTS",frame.shape)
-
-
+    
+    def runPressure(self,name,FORCE):
+        self.FORCE=FORCE
         #####################
         #Experiment hyperparameters
         ####################
@@ -277,15 +209,12 @@ class experiment_pressure:
         def runTrial(SAVER,radius):
                     global sensor
                     global filename
-                    c.reset_trial() #return to center position
-                    c.move(EDGE_VALUE+0,50,50-FORCE,0)
+                    self.c.reset_trial() #return to center position
+                    self.c.move(EDGE_VALUE+0,50,50-self.FORCE,0)
                     #move sensor across surface
                     t1=time.time()
                     if not self.presstip:
-                        ret, frame = cap.read()
-                        if not ret:
-                            print("incorrect")
-                            frame=np.zeros((100,100))
+                        frame=self.getCamera()
                     else:
                         frame=np.array(list(self.B.getSensor(type_="round",num=16)))
                     ar=np.zeros((50,*frame.shape),dtype=np.uint8)
@@ -293,12 +222,9 @@ class experiment_pressure:
                     for i,t in enumerate(np.arange(0,5,0.1)):
                         x = radius * np.sin(t)
                         y = radius * np.cos(t)
-                        c.move(x,y,increase*-1,0)
+                        self.c.move(x,y,increase*-1,0)
                         if not self.presstip:
-                            ret, frame = cap.read()
-                            if not ret:
-                                print("incorrect")
-                                frame=np.zeros((100,100))
+                            frame=self.getCamera()
                         else:
                             frame=np.array(list(self.B.getSensor(type_="round",num=16)))
                         ar[i]=frame
@@ -310,7 +236,7 @@ class experiment_pressure:
                         increase+=0.01
                     return ar
                     
-        data=np.zeros((num_experiments,num_of_trials,len(np.arange(10,400,40)),50,*frame.shape),dtype=np.uint8)
+        data=np.zeros((num_experiments,num_of_trials,len(np.arange(10,400,40)),50,*self.frame.shape),dtype=np.uint8)
         for exp in range(num_experiments):
             for trial in range(num_of_trials): #gives you the ability to average over number of trials
                 for i,y in enumerate(np.arange(10,100,10)): #chose radius
@@ -324,14 +250,14 @@ class experiment_pressure:
 
                             dat=runTrial(experiment,y) #send vector through
                             data[exp][trial][i]=dat.copy()
-                            c.move(0,0,500,0)
+                            self.c.move(0,0,500,0)
                         except KeyboardInterrupt:
-                            c.reset_trial()
-                            c.move(0,0,1000,0)
+                            self.c.reset_trial()
+                            self.c.move(0,0,1000,0)
                             print("Paused... do you want to continue (ENTER yes ctrl-C no)")
                             input(">")
                 if trial%10==0:
-                    np.save(path_to_save+"/"+name,data) #constant backups
-
+                    np.save(self.path_to_save+"/"+name,data) #constant backups
+        del data
         #plt.show()
         print("TOTAL EXECUTION TIME:",(time.time()-starttime)/(60*60),"hours")
--- Code/Examples/PC examples/scheduler.py
+++ Code/Examples/PC examples/scheduler.py
@@ -1,31 +1,25 @@
 #from TacTip import experiment
 from efficient_image_record import experiment
-from efficient_image_record import experiment_circle as experiment1
-from efficient_image_record import experiment_pressure as experiment2
 import time
+pe=-480
+#-480 for normal -880 for Foam -230 for cork and carpets, -500 flat
+
 t1=time.time()
-e1=experiment("TTA_Cork_P60",60)
-del e1
-e1=experiment("TTA_Cork_P80",80)
-del e1
-e1=experiment("TTA_Cork_P100",100)
-del e1 
+e1=experiment(Pressure_extra=pe)
+e1.runLinear("TTB/TTB_LacedMatt_P60",60)
+e1.runLinear("TTB/TTB_LacedMatt_P80",80)
+e1.runLinear("TTB/TTB_LacedMatt_P100",100)
 
 #circle
-e1=experiment1("circle_TTA_Cork_P60",60)
-del e1
-e1=experiment1("circle_TTA_Cork_P80",80)
-del e1
-e1=experiment1("circle_TTA_Cork_P100",100)
-del e1 
+e1.runCircle("TTB/non_linear/circle_TTB_LacedMatt_P60",60)
+e1.runCircle("TTB/non_linear/circle_TTB_LacedMatt_P80",80)
+e1.runCircle("TTB/non_linear/circle_TTB_LacedMatt_P100",100)
 
 #pressure
-e1=experiment2("pressure_TTA_Cork_P60",60)
-del e1
-e1=experiment2("pressure_TTA_Cork_P80",80)
-del e1
-e1=experiment2("pressure_TTA_Cork_P100",100)
-del e1 
+e1.runPressure("TTB/non_linear/pressure_TTB_LacedMatt_P60",60)
+e1.runPressure("TTB/non_linear/pressure_TTB_LacedMatt_P80",80)
+e1.runPressure("TTB/non_linear/pressure_TTB_LacedMatt_P100",100)
+
 t2=time.time()
 
 print("TOTAL TIME",(t2-t1)/(60*60),"Hours")
